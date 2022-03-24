2b7b62e16c9fce2368f4cebbcc207bb0f7dcd96f
Miguel Tejeda <migueltejedamatos@gmail.com>
2022-03-24 04:57:18 -0400

    yo

---
+++ index.js
@@ -0,0 +1,51 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const oracledb = require('oracledb')
+const dbConfig = require('./dbconfig.js')
+
+app.get('/',(request,response)=>{
+    response.send('<h1>yo</h1>')
+})
+app.get('/states',(request,response)=>{
+    response.send('<h1>states</h1>')
+})
+app.get('/states/:state',(request,response)=>{
+    const state = request.params.state
+    response.send(`<h1>${state}</h1>`)
+})
+app.get('/crimes',(request,response)=>{
+    response.send('<h1>crimes</h1>')
+})
+app.get('/crimes/:crime',(request,response)=>{
+    const crime = request.params.crime
+    response.send(`<h1>${crime}</h1>`)
+})
+//--------------------------------ORACLE
+oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
+
+const mypw = process.env.NODE_ORACLEDB_PASSWORD
+
+async function run() {
+
+  let connection;
+
+  try {
+    connection = await oracledb.getConnection({ user: "migueltejedamatos", password: mypw, connectionString: "localhost/orcl" });
+    
+    } catch (err) {
+    console.error(err);
+  }
+}
+run()
+//------------------------------------------
+/*
+todo: async/await, validate routers' parameters, set up REST client, 
+add request logger, add test env
+*/
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Running on port ${PORT}`)
+})
